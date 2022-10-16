56e8def2d149cd33fd7e21fdf9dd1e0468bf2481
Jordi Cido <jordi.cidoncha4@gmail.com>
2022-10-16 16:44:11 +0200

    2 list function completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var idCounter = 0
 
 function addTask(event) {
     var todoList = document.getElementById("todo-list")
@@ -13,6 +13,8 @@ function addTask(event) {
         p.appendChild(text)
         li.appendChild(check)
         li.appendChild(p)
+        li.id = "#"+idCounter++
+        li.onclick = changeCheckState
         todoList.appendChild(li)
         clearInputText(inputText)
     }
@@ -20,4 +22,22 @@ function addTask(event) {
 
 function clearInputText(inputText) {
     inputText.value = ''
+}
+
+function changeCheckState() {
+    var ID = this.id
+    var todoList = document.getElementById("todo-list")
+    var doneList = document.getElementById("done-list")
+    var selectedLi = document.getElementById(ID)
+    if (selectedLi.parentElement == todoList) {
+        todoList.removeChild(selectedLi)
+        selectedLi.className = "done-item"
+        selectedLi.firstElementChild.checked = true
+        doneList.appendChild(selectedLi)
+    } else {
+        doneList.removeChild(selectedLi)
+        selectedLi.className = "todo-item"
+        selectedLi.firstElementChild.checked = false
+        todoList.appendChild(selectedLi)
+    }
 }
