a9f76ee5677627b94ea2038cf35e55d881270192
Jordi Cidoncha <jordi.cidoncha@itb.cat>
2022-10-23 21:30:33 +0200

    Retrieve items from API

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
 var idCounter = 0
 
+
+async function getTodoItems() {
+    let todoItems = await fetch('http://localhost:8080/todoItems')
+    let response = await todoItems.json()
+    return response
+}
+
+getTodoItems().then( response => printItems(response))
+
+
+function printItems(response) {
+    var todoList = document.getElementById("todo-list")
+    var doneList = document.getElementById("done-list")
+    var todoItems = response.filter( it => it.done == false)
+    var doneItems = response.filter( it => it.done == true)
+    console.log(todoItems)
+    todoItems.forEach(item => {
+        var li = document.createElement("li")
+        li.classList.add("todo-item")
+        var check = document.createElement("input")
+        check.type = 'checkbox'
+        var p = document.createElement("p")
+        var text = document.createTextNode(item.text)
+        p.appendChild(text)
+        li.appendChild(check)
+        li.appendChild(p)
+        li.id = item.id
+        li.onclick = changeCheckState
+        todoList.appendChild(li)
+    });
+    doneItems.forEach(item => {
+        var li = document.createElement("li")
+        li.classList.add("done-item")
+        var check = document.createElement("input")
+        check.type = 'checkbox'
+        var p = document.createElement("p")
+        var text = document.createTextNode(item.text)
+        p.appendChild(text)
+        li.appendChild(check)
+        li.appendChild(p)
+        li.id = item.id
+        li.onclick = changeCheckState
+        li.firstElementChild.checked = true
+        doneList.appendChild(li)
+    });
+}
+
 function addTask(event) {
     var todoList = document.getElementById("todo-list")
     var inputText = document.getElementById("input-task")
@@ -40,10 +87,4 @@ function changeCheckState() {
         selectedLi.firstElementChild.checked = false
         todoList.appendChild(selectedLi)
     }
-}
-
-function getTasks() {
-    fetch('http://localhost:8080/todoItems')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
 }
